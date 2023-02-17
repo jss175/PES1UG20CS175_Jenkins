c45df4f4b9ca6ba7c595d6a5c25b97fdca65b2a1
jasalsadera <92576297+jss175@users.noreply.github.com>
2023-02-17 15:02:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "This is PESUG20CS175\n";
+  cout << "This is PES1UG20CS175\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
